955e551bfe15c24432b7d7cb8b81e957f535f732
Ahnaf Kazi <33816349+AhnafK@users.noreply.github.com>
2023-04-17 02:40:27 -0400

    Listing some flights

--- auth.py
+++ auth.py
@@ -22,20 +22,24 @@ def loginAuth():
     email = request.form['email']
     password = request.form['password']
     cursor = conn.cursor()
-    query = 'SELECT * FROM customer WHERE email = %s and password = %s'
+    query = 'SELECT email, first_name, last_name FROM customer WHERE email = %s and password = %s'
     cursor.execute(query, (email, password))
     data = cursor.fetchone()
+    query = 'SELECT * FROM flight'
+    cursor.execute(query)
+    flights = cursor.fetchall()
     cursor.close()
     if(data):
         session['user'] = {'email': data['email'], 'first_name': data['first_name'], 'last_name': data['last_name']}
+        return render_template('home.html', name=session['user']['first_name'] + ' ' + session['user']['last_name'], flights=flights)
     else:
         error = 'Invalid login or username'
-    return render_template('home.html', name=session['user']['first_name'] + ' ' + session['user']['last_name'])
+        return render_template('login.html', error=error)
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout<p>"
 
-@auth.route('/sign-up')
+@auth.route('/register')
 def sign_up():
-    return "<p>Sign Up<p>"
+    return render_template('register.html')

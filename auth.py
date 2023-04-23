4ac0599f3f900da0b160c0574bcd34c4f1b777ac
Ahnaf Kazi <33816349+AhnafK@users.noreply.github.com>
2023-04-23 06:09:36 -0400

    Registration and encryption added, as well as homepage loading flights by default

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, session, url_for, redirect
 import pymysql.cursors
+from hashlib import md5
 
 auth = Blueprint('auth',__name__)
 
@@ -11,26 +12,47 @@ conn = pymysql.connect(host='localhost',
                         charset='utf8mb4',
                         cursorclass=pymysql.cursors.DictCursor)
 
+@auth.route('/')
+def home():
+    cursor = conn.cursor()
+    query = 'SELECT * FROM flight'
+    cursor.execute(query)
+    flights = cursor.fetchall()
+    cursor.close()
+    return render_template('home.html', flights=flights)
+
 @auth.route('/login')
 def login():
-    cursor = conn.cursor()
-    cursor.close()
     return render_template('login.html')
 
 @auth.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
     email = request.form['email']
     password = request.form['password']
+
+    
     cursor = conn.cursor()
-    query = 'SELECT email, first_name, last_name FROM customer WHERE email = %s and password = %s'
-    cursor.execute(query, (email, password))
-    data = cursor.fetchone()
     query = 'SELECT * FROM flight'
     cursor.execute(query)
     flights = cursor.fetchall()
+
+    #legacy code since our test cases aren't encrypted
+    query = 'SELECT email, first_name, last_name FROM customer WHERE email = %s and password = %s'
+    cursor.execute(query, (email, password))
+    user = cursor.fetchone()
+    
     cursor.close()
-    if(data):
-        session['user'] = {'email': data['email'], 'first_name': data['first_name'], 'last_name': data['last_name']}
+    if(user):
+        session['user'] = {'email': user['email'], 'first_name': user['first_name'], 'last_name': user['last_name']}
+        return render_template('home.html', name=session['user']['first_name'] + ' ' + session['user']['last_name'], flights=flights)
+
+    cursor = conn.cursor()
+    query = 'SELECT email, first_name, last_name FROM customer WHERE email = %s and password = %s'
+    cursor.execute(query, (email, md5(password.encode()).hexdigest()))
+    user = cursor.fetchone()
+
+    if(user):
+        session['user'] = {'email': user['email'], 'first_name': user['first_name'], 'last_name': user['last_name']}
         return render_template('home.html', name=session['user']['first_name'] + ' ' + session['user']['last_name'], flights=flights)
     else:
         error = 'Invalid login or username'
@@ -43,3 +65,46 @@ def logout():
 @auth.route('/register')
 def sign_up():
     return render_template('register.html')
+
+@auth.route('/registerAuth', methods=['GET', 'POST'])
+def registerAuth():
+    
+    password = request.form['password']
+    confirm_password = request.form['confirm password']
+
+    if(password != confirm_password):
+        error = "Passwords don't match"
+        return render_template('register.html', error=error)
+    password = md5(password.encode()).hexdigest()
+
+    email = request.form['email']
+    first_name = request.form['first name']
+    last_name = request.form['last name']
+    date_of_birth = request.form['date of birth']
+    address = request.form['address']
+    building_number = address.split()[0]
+    apartment_number = request.form['apartment number']
+    if apartment_number == '':
+        apartment_number = "NULL"
+    street = ' '.join(address.split()[1:])
+    city = request.form['city']
+    state = request.form['state']
+    zip_code = request.form['zip code']
+    passport_number = request.form['passport number']
+    passport_expiration = request.form['passport expiration date']
+    phone_number = request.form['phone number']
+    cursor = conn.cursor()
+    query = 'SELECT * FROM customer WHERE email = %s'
+    cursor.execute(query, (email))
+    data = cursor.fetchone()
+    error = None
+    if(data):
+        error = "This user already exists"
+        return render_template('register.html', error=error)
+    else:
+        ins = 'INSERT INTO customer VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        cursor.execute(ins, (email, last_name, first_name, date_of_birth, password, building_number, street, apartment_number, city, state, zip_code, passport_number, passport_expiration, phone_number))
+        conn.commit()
+        cursor.close()
+        return render_template('login.html')
+    
